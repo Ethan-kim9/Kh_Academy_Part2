895d04be3a5c0df8fc11878e46b609ccf18942c8
Ethan_kim <hey0507@gmail.com>
2021-01-18 12:40:01 +0900

    lunch break

--- forEclipse/kh-spring-aop/src/aop_step3/Advice.java
+++ forEclipse/kh-spring-aop/src/aop_step3/Advice.java
@@ -1,5 +1,26 @@
 package aop_step3;
 
-public class Advice {
+import java.util.List;
 
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class Advice implements MethodInterceptor{
+
+	@Override
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		
+		Object o = null;
+		if(loginCheck()) {			
+				o = invocation.proceed();		
+		}
+		//String rs = (String)invocation.proceed();
+		//List<String> list = (List<String>)invocation.proceed();
+		return o;
+	}
+	
+	public boolean loginCheck() {
+		// 로그인 여부를 판단하는 프로세스
+		return true;
+	}
 }
